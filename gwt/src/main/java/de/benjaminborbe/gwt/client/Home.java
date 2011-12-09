19d540de9d652a31fffd8627a2377f02b9a67841
Benjamin Borbe <bborbe@rocketnews.de>
2011-12-09 10:24:36 +0100

    Add gin to Gwt

--- gwt/src/main/java/de/benjaminborbe/gwt/client/Home.java
+++ gwt/src/main/java/de/benjaminborbe/gwt/client/Home.java
@@ -1,34 +1,20 @@
 package de.benjaminborbe.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-/**
- * Entry point classes define <code>onModuleLoad()</code>.
- */
+import de.benjaminborbe.gwt.client.gin.GwtClientGinjector;
+
 public class Home implements EntryPoint {
+
+	private final GwtClientGinjector injector = GWT.create(GwtClientGinjector.class);
 
 	@Override
 	public void onModuleLoad() {
-		final Label title = new Label("Hello World");
-		final Button button = new Button("press me");
-		button.addClickHandler(new ClickHandler() {
-
-			@Override
-			public void onClick(final ClickEvent event) {
-				Window.alert("welcome");
-			}
-		});
-		final VerticalPanel vp = new VerticalPanel();
-		vp.add(title);
-		vp.add(button);
-		RootPanel.get().add(vp);
+		final Widget panel = injector.getMainPanel();
+		RootPanel.get().add(panel);
 
 	}
 }
---
+++ gwt/src/main/java/de/benjaminborbe/gwt/client/MainPanel.java
@@ -0,0 +1,35 @@
+package de.benjaminborbe.gwt.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.inject.Inject;
+
+import de.benjaminborbe.gwt.client.message.DefaultMessages;
+
+public class MainPanel extends Composite {
+
+	@Inject
+	public MainPanel(final DefaultMessages defaultMessages) {
+		final Label title = new Label(defaultMessages.getTitle());
+		final Button button = new Button(defaultMessages.getButtonText());
+		button.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(final ClickEvent event) {
+				Window.alert(defaultMessages.getWelcomeMessage());
+			}
+		});
+		final VerticalPanel vp = new VerticalPanel();
+		vp.add(title);
+		vp.add(button);
+
+		// All composites must call initWidget() in their constructors.
+		initWidget(vp);
+	}
+
+}
