3bdfe6867e3ea45927e3120ed47c06fc55c7c9f9
Benjamin Borbe <bborbe@rocketnews.de>
2011-12-14 10:57:06 +0100

    add new tcp-checks

--- monitoring/src/main/java/de/benjaminborbe/monitoring/check/TcpCheckBuilder.java
+++ monitoring/src/main/java/de/benjaminborbe/monitoring/check/TcpCheckBuilder.java
@@ -12,15 +12,42 @@ public class TcpCheckBuilder {
 
 	@Inject
 	public static void link(final Logger logger, final CheckRegistry registry) {
+		// google-website
 		{
 			final String hostname = "www.google.de";
 			final int port = 80;
 			registry.register(new TcpCheck(logger, hostname, port));
 		}
+		// dev-cassandra
 		{
 			final String hostname = "dev.rp.seibert-media.net";
 			final int port = 9160;
 			registry.register(new TcpCheck(logger, hostname, port));
 		}
+		// mysql
+		{
+			final String hostname = "localhost";
+			final int port = 3306;
+			registry.register(new TcpCheck(logger, hostname, port));
+		}
+		// cassandra
+		{
+			final String hostname = "localhost";
+			final int port = 9160;
+			registry.register(new TcpCheck(logger, hostname, port));
+		}
+		// activemq
+		{
+			final String hostname = "localhost";
+			final int port = 61616;
+			registry.register(new TcpCheck(logger, hostname, port));
+		}
+		// activemq-admin
+		{
+			final String hostname = "localhost";
+			final int port = 8161;
+			registry.register(new TcpCheck(logger, hostname, port));
+		}
+
 	}
 }
