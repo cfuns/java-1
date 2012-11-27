ec983bc68bc93a77f7a8707255800fa25529e200
Benjamin Borbe <bborbe@rocketnews.de>
2012-11-27 11:17:01 +0100

    fix

--- storage/src/main/java/de/benjaminborbe/storage/util/StorageImporter.java
+++ storage/src/main/java/de/benjaminborbe/storage/util/StorageImporter.java
@@ -2,6 +2,8 @@ package de.benjaminborbe.storage.util;
 
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.cassandra.thrift.Column;
 import org.apache.cassandra.thrift.ColumnParent;
@@ -10,7 +12,12 @@ import org.apache.cassandra.thrift.Cassandra.Iface;
 import org.apache.cassandra.thrift.InvalidRequestException;
 import org.apache.cassandra.thrift.TimedOutException;
 import org.apache.cassandra.thrift.UnavailableException;
+import org.apache.cassandra.utils.Hex;
 import org.apache.thrift.TException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 
 import com.google.inject.Inject;
@@ -27,8 +34,9 @@ public class StorageImporter {
 		this.storageConnectionPool = storageConnectionPool;
 	}
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public void importJson(final String keySpace, final String columnFamily, final String jsonContent) throws StorageConnectionPoolException, InvalidRequestException, TException,
-			UnsupportedEncodingException, UnavailableException, TimedOutException {
+			UnsupportedEncodingException, UnavailableException, TimedOutException, ParseException {
 		StorageConnection connection = null;
 		try {
 			connection = storageConnectionPool.getConnection();
@@ -39,18 +47,38 @@ public class StorageImporter {
 			final ColumnParent column_parent = new ColumnParent(columnFamily);
 			final String encoding = "UTF-8";
 
-			// TODO bborbe
-			final byte[] key = null;
-			final String columnName = null;
-			final String columnValue = null;
-			final long timestamp = 0;
+			final JSONParser parser = new JSONParser();
+			final Object object = parser.parse(jsonContent);
+			if (object instanceof JSONObject) {
+				final JSONObject root = (JSONObject) object;
+				final Set<Entry> entries = root.entrySet();
+				for (final Entry e : entries) {
+					final Object keyObject = e.getKey();
+					final Object fieldsObject = e.getValue();
+					if (keyObject instanceof String && fieldsObject instanceof JSONArray) {
+						final JSONArray fields = (JSONArray) fieldsObject;
+						final byte[] key = Hex.hexToBytes((String) keyObject);
 
-			final Column column = new Column(ByteBuffer.wrap(columnName.getBytes(encoding)));
-			column.setValue(ByteBuffer.wrap(columnValue.getBytes(encoding)));
-			column.setTimestamp(timestamp);
-			final ConsistencyLevel consistency_level = ConsistencyLevel.ONE;
+						for (final Object valuesObject : fields) {
+							final JSONArray values = (JSONArray) valuesObject;
+							if (values.size() == 3) {
 
-			client.insert(ByteBuffer.wrap(key), column_parent, column, consistency_level);
+								final String columnName = (String) values.get(0);
+								final String columnValue = (String) values.get(1);
+								final long timestamp = (Long) values.get(2);
+
+								logger.info("insert key: " + new String(key, "UTF-8") + " cn: " + columnName + " cv: " + columnValue + " time: " + timestamp);
+								final Column column = new Column(ByteBuffer.wrap(columnName.getBytes(encoding)));
+								column.setValue(ByteBuffer.wrap(columnValue.getBytes(encoding)));
+								column.setTimestamp(timestamp);
+								final ConsistencyLevel consistency_level = ConsistencyLevel.ONE;
+
+								client.insert(ByteBuffer.wrap(key), column_parent, column, consistency_level);
+							}
+						}
+					}
+				}
+			}
 
 		}
 		finally {
---
+++ storage/src/test/java/de/benjaminborbe/storage/util/StorageImporterIntegrationTest.java
@@ -0,0 +1,81 @@
+package de.benjaminborbe.storage.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.inject.Injector;
+
+import de.benjaminborbe.storage.guice.StorageModulesMock;
+import de.benjaminborbe.tools.guice.GuiceInjectorBuilder;
+
+public class StorageImporterIntegrationTest {
+
+	private static boolean notFound;
+
+	@BeforeClass
+	public static void setUpClass() {
+		final Injector injector = GuiceInjectorBuilder.getInjector(new StorageModulesMock());
+		final StorageConfig config = injector.getInstance(StorageConfig.class);
+
+		final Socket socket = new Socket();
+		final SocketAddress endpoint = new InetSocketAddress(config.getHost(), config.getPort());
+		try {
+			socket.connect(endpoint, 500);
+
+			notFound = !socket.isConnected();
+			notFound = false;
+		}
+		catch (final IOException e) {
+			notFound = true;
+		}
+	}
+
+	@Before
+	public void setUp() {
+		if (notFound)
+			return;
+		StorageTestUtil.setUp();
+	}
+
+	@After
+	public void tearDown() {
+		if (notFound)
+			return;
+		StorageTestUtil.tearDown();
+	}
+
+	@Test
+	public void testImport() throws Exception {
+		if (notFound)
+			return;
+		final Injector injector = GuiceInjectorBuilder.getInjector(new StorageModulesMock());
+		final StorageConfig config = injector.getInstance(StorageConfig.class);
+		final StorageDaoUtil daoUtil = injector.getInstance(StorageDaoUtil.class);
+		final StorageImporter importer = injector.getInstance(StorageImporter.class);
+
+		// leer db
+		assertEquals(0, daoUtil.count(config.getKeySpace(), StorageTestUtil.COLUMNFAMILY));
+
+		final StringWriter jsonContent = new StringWriter();
+		jsonContent.append("{\n");
+		jsonContent
+				.append("\"63623334663430632d343930372d343661392d626636652d333765366635633735376338\": [[\"created\",\"1352884792199\",1352884792199], [\"id\",\"cb34f40c-4907-46a9-bf6e-37e6f5c757c8\",1352884792199], [\"modified\",\"1352884792199\",1352884792199], [\"name\",\"someday\",1352884856172000], [\"owner\",\"bborbe\",1352884792199]],\n");
+		jsonContent
+				.append("\"62636564633435302d663834382d336538392d623134342d353036396163646333646439\": [[\"created\",\"1352312862914\",1352312862916], [\"id\",\"bcedc450-f848-3e89-b144-5069acdc3dd9\",1352312862915], [\"modified\",\"1352312862914\",1352312862921], [\"name\",\"wow\",1352312862918], [\"owner\",\"bborbe\",1352312862919]]\n");
+		jsonContent.append("}\n");
+
+		importer.importJson(config.getKeySpace(), StorageTestUtil.COLUMNFAMILY, jsonContent.toString());
+
+		assertEquals(2, daoUtil.count(config.getKeySpace(), StorageTestUtil.COLUMNFAMILY));
+	}
+}
