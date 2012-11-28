8735565373669670d79ec21e901881e70d740627
Benjamin Borbe <bborbe@rocketnews.de>
2012-11-29 00:01:51 +0100

    fix

--- xmpp/src/test/java/de/benjaminborbe/xmpp/guice/XmppOsgiModuleMock.java
+++ xmpp/src/test/java/de/benjaminborbe/xmpp/guice/XmppOsgiModuleMock.java
@@ -8,6 +8,8 @@ import com.google.inject.Singleton;
 
 import de.benjaminborbe.authentication.api.AuthenticationService;
 import de.benjaminborbe.authentication.mock.AuthenticationServiceMock;
+import de.benjaminborbe.authorization.api.AuthorizationService;
+import de.benjaminborbe.authorization.mock.AuthorizationServiceMock;
 import de.benjaminborbe.configuration.api.ConfigurationService;
 import de.benjaminborbe.configuration.mock.ConfigurationServiceMock;
 import de.benjaminborbe.tools.osgi.mock.ExtHttpServiceMock;
@@ -18,6 +20,7 @@ public class XmppOsgiModuleMock extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(AuthenticationService.class).to(AuthenticationServiceMock.class).in(Singleton.class);
+		bind(AuthorizationService.class).to(AuthorizationServiceMock.class).in(Singleton.class);
 		bind(ConfigurationService.class).to(ConfigurationServiceMock.class).in(Singleton.class);
 		bind(LogService.class).to(LogServiceMock.class).in(Singleton.class);
 		bind(ExtHttpService.class).to(ExtHttpServiceMock.class).in(Singleton.class);
