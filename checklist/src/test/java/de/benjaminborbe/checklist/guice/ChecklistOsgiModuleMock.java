0429360cbf0b3393f333d1f5b8f34650dec676ac
Benjamin Borbe <bborbe@rocketnews.de>
2012-12-16 18:45:10 +0100

    fix

--- checklist/src/test/java/de/benjaminborbe/checklist/guice/ChecklistOsgiModuleMock.java
+++ checklist/src/test/java/de/benjaminborbe/checklist/guice/ChecklistOsgiModuleMock.java
@@ -8,6 +8,8 @@ import com.google.inject.Singleton;
 
 import de.benjaminborbe.authentication.api.AuthenticationService;
 import de.benjaminborbe.authentication.mock.AuthenticationServiceMock;
+import de.benjaminborbe.authorization.api.AuthorizationService;
+import de.benjaminborbe.authorization.mock.AuthorizationServiceMock;
 import de.benjaminborbe.storage.api.StorageService;
 import de.benjaminborbe.storage.mock.StorageServiceMock;
 import de.benjaminborbe.tools.osgi.mock.ExtHttpServiceMock;
@@ -19,6 +21,7 @@ public class ChecklistOsgiModuleMock extends AbstractModule {
 	protected void configure() {
 		bind(StorageService.class).to(StorageServiceMock.class).in(Singleton.class);
 		bind(AuthenticationService.class).to(AuthenticationServiceMock.class).in(Singleton.class);
+		bind(AuthorizationService.class).to(AuthorizationServiceMock.class).in(Singleton.class);
 		bind(LogService.class).to(LogServiceMock.class).in(Singleton.class);
 		bind(ExtHttpService.class).to(ExtHttpServiceMock.class).in(Singleton.class);
 	}
