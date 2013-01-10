7135e68cee44b5e1f411a8d427854c5bac0dc0d8
Benjamin Borbe <bborbe@rocketnews.de>
2013-01-10 15:28:49 +0100

    fix

--- projectile_gui/src/main/java/de/benjaminborbe/projectile/gui/servlet/ProjectileGuiReportServlet.java
+++ projectile_gui/src/main/java/de/benjaminborbe/projectile/gui/servlet/ProjectileGuiReportServlet.java
@@ -88,9 +88,9 @@ public class ProjectileGuiReportServlet extends WebsiteHtmlServlet {
 			final ProjectileSlacktimeReport report = projectileService.getSlacktimeReport(sessionIdentifier);
 
 			if (report != null) {
-				widgets.add(createBlock("Week", report.getWeekIntern(), report.getWeekExtern(), report.getWeekBillable()));
-				widgets.add(createBlock("Month", report.getMonthIntern(), report.getMonthExtern(), report.getMonthBillable()));
-				widgets.add(createBlock("Year", report.getYearIntern(), report.getYearExtern(), report.getYearBillable()));
+				widgets.add(createBlock("Week", report.getWeekIntern(), report.getWeekExtern(), report.getWeekBillable(), report.getWeekTarget()));
+				widgets.add(createBlock("Month", report.getMonthIntern(), report.getMonthExtern(), report.getMonthBillable(), report.getMonthTarget()));
+				widgets.add(createBlock("Year", report.getYearIntern(), report.getYearExtern(), report.getYearBillable(), report.getYearTarget()));
 			}
 			else {
 				widgets.add("no data found");
@@ -109,18 +109,19 @@ public class ProjectileGuiReportServlet extends WebsiteHtmlServlet {
 		}
 	}
 
-	private Widget createBlock(final String name, final Double intern, final Double extern, final Double billable) {
+	private Widget createBlock(final String name, final Double intern, final Double extern, final Double billable, final Double target) {
 		final ListWidget widgets = new ListWidget();
 		widgets.add(new H2Widget(name));
-		if (extern != null && intern != null && billable != null && (extern + intern > 0)) {
+		if (extern != null && intern != null && (extern + intern > 0)) {
 			final double total = extern + intern;
 			final double externPercent = (extern / total) * 100;
-			final double billableExternPercent = (billable / total) * 100;
+			final Double billableExternPercent = billable != null && extern > 0 ? ((billable / extern) * 100) : null;
 			final DecimalFormat df = new DecimalFormat("#####0.0h");
 			widgets.add("Extern: ");
 			widgets.add(df.format(externPercent) + "%");
+			widgets.add(" ");
 			widgets.add("Billable: ");
-			widgets.add(df.format(billableExternPercent) + "%");
+			widgets.add(billableExternPercent != null ? (df.format(billableExternPercent) + "%") : "~");
 			widgets.add(" ");
 			widgets.add("(");
 			widgets.add("total: " + df.format(total));
@@ -128,6 +129,12 @@ public class ProjectileGuiReportServlet extends WebsiteHtmlServlet {
 			widgets.add("extern: " + df.format(extern));
 			widgets.add(" ");
 			widgets.add("intern: " + df.format(intern));
+			widgets.add(" ");
+			widgets.add("billable: ");
+			widgets.add(billable != null ? df.format(billable) : "~");
+			widgets.add(" ");
+			widgets.add("target: ");
+			widgets.add(target != null ? df.format(target) : "~");
 			widgets.add(")");
 		}
 		else {
