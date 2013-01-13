5f8fdd4dd36561468eba79a201d37a0ee148e4d5
Benjamin Borbe <bborbe@rocketnews.de>
2013-01-13 14:47:56 +0100

    add test

---
+++ analytics/src/test/java/de/benjaminborbe/analytics/util/AnalyticsReportValueIteratorFillMissingValuesUnitTest.java
@@ -0,0 +1,14 @@
+package de.benjaminborbe.analytics.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnalyticsReportValueIteratorFillMissingValuesUnitTest {
+
+	@Test
+	public void testFill() throws Exception {
+		// TODO test fill missing
+		assertTrue(true);
+	}
+}
