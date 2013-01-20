1e2f60d2c839c4e629578d99e8bbd4a37c6a3cb4
Benjamin Borbe <bborbe@rocketnews.de>
2013-01-20 02:01:46 +0100

    fix encoding

---
+++ bridge/lib/src/main/java/de/benjaminborbe/bridge/filter/SetCharacterEncodingFilter.java
@@ -0,0 +1,37 @@
+package de.benjaminborbe.bridge.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class SetCharacterEncodingFilter implements Filter {
+
+	public SetCharacterEncodingFilter() {
+	}
+
+	@Override
+	public void init(final FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+		try {
+			if (request.getCharacterEncoding() == null) {
+				request.setCharacterEncoding("UTF-8");
+			}
+		}
+		finally {
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
