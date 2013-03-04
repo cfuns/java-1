349f6ef72b88293b13bcf07ba0a7407402e39348
Benjamin Borbe <bborbe@rocketnews.de>
2013-03-04 19:27:37 +0100

    bookmark fix

---
+++ bookmark_gui/src/main/java/de/benjaminborbe/bookmark/gui/util/BookmarkGuiWebsiteHtmlServlet.java
@@ -0,0 +1,67 @@
+package de.benjaminborbe.bookmark.gui.util;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+
+import com.google.inject.Provider;
+
+import de.benjaminborbe.authentication.api.AuthenticationService;
+import de.benjaminborbe.authentication.api.LoginRequiredException;
+import de.benjaminborbe.authorization.api.AuthorizationService;
+import de.benjaminborbe.authorization.api.AuthorizationServiceException;
+import de.benjaminborbe.authorization.api.PermissionDeniedException;
+import de.benjaminborbe.authorization.api.PermissionIdentifier;
+import de.benjaminborbe.bookmark.api.BookmarkService;
+import de.benjaminborbe.cache.api.CacheService;
+import de.benjaminborbe.html.api.HttpContext;
+import de.benjaminborbe.navigation.api.NavigationWidget;
+import de.benjaminborbe.tools.date.CalendarUtil;
+import de.benjaminborbe.tools.date.TimeZoneUtil;
+import de.benjaminborbe.tools.url.UrlUtil;
+import de.benjaminborbe.tools.util.ParseUtil;
+import de.benjaminborbe.website.servlet.WebsiteHtmlServlet;
+
+public abstract class BookmarkGuiWebsiteHtmlServlet extends WebsiteHtmlServlet {
+
+	private static final long serialVersionUID = -7305831810086217136L;
+
+	private final AuthorizationService authorizationService;
+
+	public BookmarkGuiWebsiteHtmlServlet(
+			final Logger logger,
+			final CalendarUtil calendarUtil,
+			final TimeZoneUtil timeZoneUtil,
+			final ParseUtil parseUtil,
+			final NavigationWidget navigationWidget,
+			final AuthenticationService authenticationService,
+			final AuthorizationService authorizationService,
+			final Provider<HttpContext> httpContextProvider,
+			final UrlUtil urlUtil,
+			final CacheService cacheService) {
+		super(logger, calendarUtil, timeZoneUtil, parseUtil, navigationWidget, authenticationService, authorizationService, httpContextProvider, urlUtil, cacheService);
+		this.authorizationService = authorizationService;
+	}
+
+	@Override
+	public boolean isAdminRequired() {
+		return false;
+	}
+
+	@Override
+	protected void doCheckPermission(final HttpServletRequest request, final HttpServletResponse response, final HttpContext context) throws ServletException, IOException,
+			PermissionDeniedException, LoginRequiredException {
+		try {
+			final PermissionIdentifier permissionIdentifier = authorizationService.createPermissionIdentifier(BookmarkService.PERMISSION);
+			authorizationService.existsPermission(permissionIdentifier);
+		}
+		catch (final AuthorizationServiceException e) {
+			throw new PermissionDeniedException(e);
+		}
+	}
+
+}
