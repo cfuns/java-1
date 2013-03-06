87f2e3a982e611c74b8de010b4c58bd3a93d7bd8
Benjamin Borbe <bborbe@rocketnews.de>
2013-03-06 22:07:45 +0100

    fix

--- note/src/main/java/de/benjaminborbe/note/guice/NoteModule.java
+++ note/src/main/java/de/benjaminborbe/note/guice/NoteModule.java
@@ -6,6 +6,8 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
 import de.benjaminborbe.note.api.NoteService;
+import de.benjaminborbe.note.dao.NoteDao;
+import de.benjaminborbe.note.dao.NoteDaoStorage;
 import de.benjaminborbe.note.service.NoteServiceImpl;
 import de.benjaminborbe.tools.log.LoggerSlf4Provider;
 
@@ -13,6 +15,7 @@ public class NoteModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
+		bind(NoteDao.class).to(NoteDaoStorage.class).in(Singleton.class);
 		bind(NoteService.class).to(NoteServiceImpl.class).in(Singleton.class);
 		bind(Logger.class).toProvider(LoggerSlf4Provider.class).in(Singleton.class);
 	}
