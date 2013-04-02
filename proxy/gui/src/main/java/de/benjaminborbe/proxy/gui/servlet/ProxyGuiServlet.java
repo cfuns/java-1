67daf35ac15cd9b34f301a2dcd9fcbe2f1bacda0
Benjamin Borbe <bborbe@rocketnews.de>
2013-04-02 23:53:03 +0200

    proxy

--- proxy/gui/src/main/java/de/benjaminborbe/proxy/gui/servlet/ProxyGuiServlet.java
+++ proxy/gui/src/main/java/de/benjaminborbe/proxy/gui/servlet/ProxyGuiServlet.java
@@ -4,41 +4,43 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 import de.benjaminborbe.authentication.api.AuthenticationService;
+import de.benjaminborbe.authentication.api.LoginRequiredException;
 import de.benjaminborbe.authorization.api.AuthorizationService;
-import de.benjaminborbe.cache.api.CacheService;
+import de.benjaminborbe.authorization.api.PermissionDeniedException;
 import de.benjaminborbe.html.api.HttpContext;
-import de.benjaminborbe.navigation.api.NavigationWidget;
 import de.benjaminborbe.tools.date.CalendarUtil;
 import de.benjaminborbe.tools.date.TimeZoneUtil;
 import de.benjaminborbe.tools.url.UrlUtil;
-import de.benjaminborbe.tools.util.ParseUtil;
-import de.benjaminborbe.website.servlet.WebsiteHtmlServlet;
+import de.benjaminborbe.website.servlet.WebsiteServlet;
 import org.slf4j.Logger;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @Singleton
-public class ProxyGuiServlet extends WebsiteHtmlServlet {
+public class ProxyGuiServlet extends WebsiteServlet {
 
 	private static final long serialVersionUID = 1328676176772634649L;
 
-	private static final String TITLE = "Proxy";
-
 	@Inject
-	public ProxyGuiServlet(
-		final Logger logger,
-		final CalendarUtil calendarUtil,
-		final TimeZoneUtil timeZoneUtil,
-		final ParseUtil parseUtil,
-		final AuthenticationService authenticationService,
-		final NavigationWidget navigationWidget,
-		final Provider<HttpContext> httpContextProvider,
-		final UrlUtil urlUtil,
-		final AuthorizationService authorizationService,
-		final CacheService cacheService) {
-		super(logger, calendarUtil, timeZoneUtil, parseUtil, navigationWidget, authenticationService, authorizationService, httpContextProvider, urlUtil, cacheService);
+	public ProxyGuiServlet(final Logger logger, final UrlUtil urlUtil, final AuthenticationService authenticationService, final AuthorizationService authorizationService, final CalendarUtil calendarUtil, final TimeZoneUtil timeZoneUtil, final Provider<HttpContext> httpContextProvider) {
+		super(logger, urlUtil, authenticationService, authorizationService, calendarUtil, timeZoneUtil, httpContextProvider);
 	}
 
 	@Override
-	protected String getTitle() {
-		return TITLE;
+	protected void doService(final HttpServletRequest request, final HttpServletResponse response, final HttpContext context) throws ServletException, IOException, PermissionDeniedException, LoginRequiredException {
+
+	}
+
+	@Override
+	public boolean isAdminRequired() {
+		return false;
+	}
+
+	@Override
+	public boolean isLoginRequired() {
+		return false;
 	}
 }
