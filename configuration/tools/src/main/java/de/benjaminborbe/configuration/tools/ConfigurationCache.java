ba060b61730386ae533aea10f5be5c0cc0a682e1
Benjamin Borbe <bborbe@rocketnews.de>
2013-04-28 18:49:25 +0200

    config cache

---
+++ configuration/tools/src/main/java/de/benjaminborbe/configuration/tools/ConfigurationCache.java
@@ -0,0 +1,8 @@
+package de.benjaminborbe.configuration.tools;
+
+import de.benjaminborbe.configuration.api.ConfigurationDescription;
+import de.benjaminborbe.lib.cache.Cache;
+
+public interface ConfigurationCache extends Cache<ConfigurationDescription, String> {
+
+}
---
+++ lib/cache/src/main/java/de/benjaminborbe/lib/cache/Cache.java
@@ -0,0 +1,10 @@
+package de.benjaminborbe.lib.cache;
+
+public interface Cache<Key, Value> {
+
+	Value get(Key configurationDescription);
+
+	void put(Key configurationDescription, Value value);
+
+	void clear();
+}
