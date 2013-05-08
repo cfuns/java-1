7661a7359859adee63ce9ac3983ba248e088405c
Benjamin Borbe <bborbe@rocketnews.de>
2013-05-08 22:45:34 +0200

    shorten long ids

--- message/gui/src/main/java/de/benjaminborbe/message/gui/servlet/MessageGuiMessageListServlet.java
+++ message/gui/src/main/java/de/benjaminborbe/message/gui/servlet/MessageGuiMessageListServlet.java
@@ -21,6 +21,7 @@ import de.benjaminborbe.tools.date.CalendarUtil;
 import de.benjaminborbe.tools.date.TimeZoneUtil;
 import de.benjaminborbe.tools.url.UrlUtil;
 import de.benjaminborbe.tools.util.ParseUtil;
+import de.benjaminborbe.tools.util.StringUtil;
 import de.benjaminborbe.website.servlet.RedirectException;
 import de.benjaminborbe.website.servlet.WebsiteHtmlServlet;
 import de.benjaminborbe.website.table.TableHeadWidget;
@@ -60,6 +61,8 @@ public class MessageGuiMessageListServlet extends WebsiteHtmlServlet {
 
 	private final MessageGuiLinkFactory messageGuiLinkFactory;
 
+	private final StringUtil stringUtil;
+
 	@Inject
 	public MessageGuiMessageListServlet(
 		final Logger logger,
@@ -73,7 +76,8 @@ public class MessageGuiMessageListServlet extends WebsiteHtmlServlet {
 		final AuthorizationService authorizationService,
 		final CacheService cacheService,
 		final MessageService messageService,
-		final MessageGuiLinkFactory messageGuiLinkFactory
+		final MessageGuiLinkFactory messageGuiLinkFactory,
+		final StringUtil stringUtil
 	) {
 		super(logger, calendarUtil, timeZoneUtil, parseUtil, navigationWidget, authenticationService, authorizationService, httpContextProvider, urlUtil, cacheService);
 		this.messageService = messageService;
@@ -81,6 +85,7 @@ public class MessageGuiMessageListServlet extends WebsiteHtmlServlet {
 		this.logger = logger;
 		this.calendarUtil = calendarUtil;
 		this.messageGuiLinkFactory = messageGuiLinkFactory;
+		this.stringUtil = stringUtil;
 	}
 
 	@Override
@@ -113,7 +118,7 @@ public class MessageGuiMessageListServlet extends WebsiteHtmlServlet {
 				for (final Message message : messages) {
 					final TableRowWidget row = new TableRowWidget();
 					row.addCell(asString(message.getType()));
-					row.addCell(asString(message.getId()));
+					row.addCell(stringUtil.shortenDots(asString(message.getId()), 150));
 					row.addCell(asString(message.getLockName()));
 					row.addCell(asString(calendarUtil.toDateTimeString(message.getLockTime())));
 					row.addCell(asString(calendarUtil.toDateTimeString(message.getCreated())));
