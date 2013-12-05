c2ddc7af17ebecbce09daa31ba26fbb48593d538
Benjamin Borbe <bborbe@rocketnews.de>
2013-12-05 16:50:54 +0100

    Revert chart templates

--- analytics/gui/src/main/resources/chart_data.js
+++ analytics/gui/src/main/resources/chart_data.js
@@ -1,58 +1,32 @@
-$(function () {
+$(function() {
 		$("#chart").chart({
 			template: "line_basic_2",
 			tooltips: {
-		{
-			tooltips
-		}
+		{tooltips}
 	},
-	values
-:
-{
-	{
-		values
-	}
-}
-,
+	values: {
+	{values}
+},
 legend : {
-	{
-		legend
-	}
-}
-,
+	{legend}
+},
 defaultSeries: {
 	fill: true,
-		stacked
-:
-	false,
-		highlight
-:
-	{
+		stacked: false,
+		highlight: {
 		scale: 2
-	}
-,
+	},
 	tooltip: {
 		active: true,
-			width
-	:
-		250,
-			height
-	:
-		70,
-	}
-,
+			width: 250,
+			height: 70,
+	},
 	startAnimation: {
 		active: true,
-			type
-	:
-		"grow",
-			easing
-	:
-		"bounce"
+			type: "grow",
+			easing: "bounce"
 	}
 }
-})
-;
+});
 
-})
-;
+});
--- analytics/gui/src/main/resources/chart_template.js
+++ analytics/gui/src/main/resources/chart_template.js
@@ -12,62 +12,34 @@ $.elycharts.templates['line_basic_2'] = {
 		}
 	},
 	series: {
-{
-	colors
-}
+{colors}
 },
 defaultAxis: {
 	labels: true
-}
-,
+},
 features: {
 	grid: {
 		draw: [true, false],
-			props
-	:
-		{
-			"stroke-dasharray"
-		:
-			"-"
+			props: {
+			"stroke-dasharray": "-"
 		}
-	}
-,
+	},
 	legend: {
 		horizontal: false,
-			width
-	:
-		250,
-			height
-	:
-		100,
-			x
-	:
-		50,
-			y
-	:
-		10,
-			dotType
-	:
-		"circle",
-			dotProps
-	:
-		{
+			width: 250,
+			height: 100,
+			x: 50,
+			y: 10,
+			dotType: "circle",
+			dotProps: {
 			stroke: "white",
-				"stroke-width"
-		:
-			2
-		}
-	,
+				"stroke-width": 2
+		},
 		borderProps: {
 			opacity: 0.3,
-				fill
-		:
-			"#c0c0c0",
-				"stroke-width"
-		:
-			0
+				fill: "#c0c0c0",
+				"stroke-width": 0
 		}
 	}
 }
-}
-;
+};
