8c6a6d0fb48908496b7e29196fb7d3e8273a43c3
Sandro Janusch <sjanusch@seibert-media.net>
2014-10-31 11:02:13 +0100

    Add Guice dep for PokerService

--- poker/table/src/main/java/de/benjaminborbe/poker/table/server/guice/PokerTableServerOsgiModule.java
+++ poker/table/src/main/java/de/benjaminborbe/poker/table/server/guice/PokerTableServerOsgiModule.java
@@ -4,6 +4,7 @@ import com.google.inject.AbstractModule;
 import de.benjaminborbe.authentication.api.AuthenticationService;
 import de.benjaminborbe.authorization.api.AuthorizationService;
 import de.benjaminborbe.configuration.api.ConfigurationService;
+import de.benjaminborbe.poker.api.PokerService;
 import org.apache.felix.http.api.ExtHttpService;
 import org.osgi.service.log.LogService;
 
@@ -18,5 +19,6 @@ public class PokerTableServerOsgiModule extends AbstractModule {
 		bind(AuthorizationService.class).toProvider(service(AuthorizationService.class).single());
 		bind(LogService.class).toProvider(service(LogService.class).single());
 		bind(ExtHttpService.class).toProvider(service(ExtHttpService.class).single());
+		bind(PokerService.class).toProvider(service(PokerService.class).single());
 	}
 }
--- poker/table/src/test/java/de/benjaminborbe/poker/table/server/guice/PokerTableServerOsgiModuleMock.java
+++ poker/table/src/test/java/de/benjaminborbe/poker/table/server/guice/PokerTableServerOsgiModuleMock.java
@@ -7,6 +7,8 @@ import de.benjaminborbe.authorization.api.AuthorizationService;
 import de.benjaminborbe.authorization.mock.AuthorizationServiceMock;
 import de.benjaminborbe.configuration.api.ConfigurationService;
 import de.benjaminborbe.configuration.mock.ConfigurationServiceMock;
+import de.benjaminborbe.poker.api.PokerService;
+import de.benjaminborbe.poker.mock.PokerServiceMock;
 import de.benjaminborbe.tools.osgi.mock.ExtHttpServiceMock;
 import de.benjaminborbe.tools.osgi.mock.LogServiceMock;
 import org.apache.felix.http.api.ExtHttpService;
@@ -23,5 +25,6 @@ public class PokerTableServerOsgiModuleMock extends AbstractModule {
 		bind(AuthorizationService.class).to(AuthorizationServiceMock.class).in(Singleton.class);
 		bind(LogService.class).to(LogServiceMock.class).in(Singleton.class);
 		bind(ExtHttpService.class).to(ExtHttpServiceMock.class).in(Singleton.class);
+		bind(PokerService.class).to(PokerServiceMock.class).in(Singleton.class);
 	}
 }
