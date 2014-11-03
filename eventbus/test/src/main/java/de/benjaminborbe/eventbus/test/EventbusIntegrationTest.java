e5be44afc3ee47b5b2bc80a0e8a567bed4ca7995
Benjamin Borbe <bborbe@rocketnews.de>
2014-11-03 16:40:02 +0100

    rename variable

--- eventbus/test/src/main/java/de/benjaminborbe/eventbus/test/EventbusIntegrationTest.java
+++ eventbus/test/src/main/java/de/benjaminborbe/eventbus/test/EventbusIntegrationTest.java
@@ -60,25 +60,25 @@ public class EventbusIntegrationTest extends TestCaseOsgi {
 	}
 
 	public void testEventbus() {
-		final EventbusService Eventbus = (EventbusService) getServiceObject(EventbusService.class.getName(), null);
-		assertNotNull(Eventbus);
-		assertEquals("de.benjaminborbe.eventbus.service.EventbusServiceImpl", Eventbus.getClass().getName());
+		final EventbusService eventbusService = (EventbusService) getServiceObject(EventbusService.class.getName(), null);
+		assertNotNull(eventbusService);
+		assertEquals("de.benjaminborbe.eventbus.service.EventbusServiceImpl", eventbusService.getClass().getName());
 
 		final List<EventbusInitializedEvent> events = new ArrayList<EventbusInitializedEvent>();
 
-		assertEquals(0, Eventbus.getHandlerCount(EventbusInitializedEvent.TYPE));
-		assertFalse(Eventbus.isEventHandled(EventbusInitializedEvent.TYPE));
-		Eventbus.addHandler(EventbusInitializedEvent.TYPE, new EventbusInitializedEventHandler() {
+		assertEquals(0, eventbusService.getHandlerCount(EventbusInitializedEvent.TYPE));
+		assertFalse(eventbusService.isEventHandled(EventbusInitializedEvent.TYPE));
+		eventbusService.addHandler(EventbusInitializedEvent.TYPE, new EventbusInitializedEventHandler() {
 
 			@Override
 			public void onInitialize(final EventbusInitializedEvent event) {
 				events.add(event);
 			}
 		});
-		assertEquals(1, Eventbus.getHandlerCount(EventbusInitializedEvent.TYPE));
-		assertTrue(Eventbus.isEventHandled(EventbusInitializedEvent.TYPE));
+		assertEquals(1, eventbusService.getHandlerCount(EventbusInitializedEvent.TYPE));
+		assertTrue(eventbusService.isEventHandled(EventbusInitializedEvent.TYPE));
 		assertEquals(0, events.size());
-		Eventbus.fireEvent(new EventbusInitializedEvent());
+		eventbusService.fireEvent(new EventbusInitializedEvent());
 		assertEquals(1, events.size());
 	}
 }
